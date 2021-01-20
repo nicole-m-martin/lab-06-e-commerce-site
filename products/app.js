f8fa619bfec4139513309fdbbca662713bfc9204
Nicole Martin <n.martinpdx@gmail.com>
2021-01-20 10:29:37 -0800

    change paths of files

--- products/app.js
+++ products/app.js
@@ -1,14 +1,20 @@
 // This is where the functions will import and the for loop is at
-import { spaceItems } from '../products/data.js';
-import { renderSpaceItems } from '../products/renderSpaceItems.js';
+import { spaceItems } from './data.js';
+import { renderSpaceItems } from './renderSpaceItems.js';
 
-
+// an empty div to inject stuff into
 const itemList = document.getElementById('space-list-products');
 
+// loop through the space items array
+// loop means do something to every item in an array
 for (let spaceItem of spaceItems) {
+    // for each space item in space items array
+
+    // feed it into the renderSpaceItems  function
+    // it returns a DOM element called spaceElement
     const spaceElement = renderSpaceItems(spaceItem);
 
-  
+    // append it to our empty list
     itemList.append(spaceElement);
   
 }
