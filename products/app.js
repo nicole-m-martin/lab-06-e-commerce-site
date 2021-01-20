f3fec882a631cf14fb49f2e8ebd14555ef66774a
Nicole Martin <n.martinpdx@gmail.com>
2021-01-19 21:43:13 -0800

    need to fix css tomorrow

--- products/app.js
+++ products/app.js
@@ -1,1 +1,15 @@
 // This is where the functions will import and the for loop is at
+import { spaceItems } from '../products/data.js';
+import { renderSpaceItems } from '../products/renderSpaceItems.js';
+
+
+const itemList = document.getElementById('space-list-products');
+
+for (let spaceItem of spaceItems) {
+    const spaceElement = renderSpaceItems(spaceItem);
+
+  
+    itemList.append(spaceElement);
+  
+}
+
