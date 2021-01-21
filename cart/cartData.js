a2cb453ad16133d2b59d10d8a7efc63ff81d1a4f
Nicole Martin <n.martinpdx@gmail.com>
2021-01-20 16:25:09 -0800

    add cart quantity to cartData.js

--- cart/cartData.js
+++ cart/cartData.js
@@ -1,17 +1,19 @@
 // This is where the cart data lists for items goes
 
-// 
-// ## How many aliens are in the cart? 
-// 1) find the id of the alien
-//  a) iterate/loop through the space items array
-//  b) check the 'category' property of each items
-//  c) if the 'category' property is 'alien', we found the 'alien'
-//  d) get the 'id' property of the 'alien' we found.
+export const cart = [
+
+    {
+        id: 1,
+        quantity: 5
+    },
+    {
+        id: 3,
+        quantity: 4
+    },
+    {
+        id: 4,
+        quantity: 2
+    }
 
 
-// 2) find an object in the cart array with an space item of 1.
-//  a) iterate/loop through the cart
-//  b) check the 'alien' property of each item
-//  c) if the 'alien' property is the same as the 'id' we found, its a match!
-//  - If no match, repeat this cart
-
+];
